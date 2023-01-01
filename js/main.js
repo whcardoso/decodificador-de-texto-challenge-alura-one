3ab87d7b9318f9c53c023e06dbec825ca627a0fb
Wallace <wallacehenriquerjbr@gmail.com>
2023-01-01 19:25:49 -0300

    Criando codificador e decodificador de mensagens Alura

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function criptografar() {
+    const texto = entrada.value;
+    const textoCriptografado = texto.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+    mostraResultado(textoCriptografado);
+}
+
+function descriptografar() {
+    const texto = entrada.value;
+    const textoDescriptografado = texto.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
+    mostraResultado(textoDescriptografado);
+}
+
+function mostraResultado(texto) {
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '<textarea type="text" id="saida" readonly>' + texto + '</textarea>' + '<button id="copiar" onclick="copiar()">Copiar</button>';
+}
+
+function copiar() {
+    let textoCopiado = document.getElementById('saida');
+    textoCopiado.select();
+    document.execCommand('copy');
+    alert('Sua mensagem foi copiada com sucesso!');
+}
+
+var entrada = document.getElementById('entrada');
+var btnCriptografar = document.getElementById('criptografar');
+var btnDescriptografar = document.getElementById('descriptografar');
+
+entrada.focus();
+btnCriptografar.onclick = criptografar;
+btnDescriptografar.onclick = descriptografar;
